0c754f2fb7d79a54cc786c98e5f093a2f1f77a67
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-06-02 17:46:58 +0800

    Add one more solution to problem 6

--- Problem_6.rb
+++ Problem_6.rb
@@ -1,5 +1,8 @@
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
+=begin
+
+# Solution #1
 sqrsum = 0
 sum = 0
 (1..100).each do |i|
@@ -11,3 +14,16 @@ sumsqr = sum ** 2
 
 diff = sqrsum - sumsqr
 puts diff.abs
+
+=end
+
+# Solution #2
+
+def Diff_SumSqr_SqrSum(n)
+  sumsqr = ((1 + n) * n / 2) ** 2
+  sqrsum = n * ((2 * n) + 1) * (n + 1) / 6
+  puts sumsqr, sqrsum
+  puts (sumsqr - sqrsum).abs
+end
+
+puts Diff_SumSqr_SqrSum(100)
