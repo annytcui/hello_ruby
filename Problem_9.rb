833302a00ead930fdd2a4b26b521f4f1cd07686f
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-06-04 15:00:09 +0800

    Improve solution

--- Problem_9.rb
+++ Problem_9.rb
@@ -1,7 +1,7 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product of abc.
 
-for a in (1..500)
+for a in (1..332)
   for b in (a..500)
     for c in (b..500)
       if a**2 + b**2 == c**2 and a+b+c == 1000
