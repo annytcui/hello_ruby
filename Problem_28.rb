da7739beb6c4a6397e4e2b06ffa207a18d2d0db2
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-06-11 08:32:07 +0800

    Improve solution #1

--- Problem_28.rb
+++ Problem_28.rb
@@ -1,21 +1,15 @@
 # Project Euler Problem 28
 
+# Solution #1
 limit = 1001 ** 2
 numbers = (1..limit).to_a
 i, j = 0, 2
 sum = 1
 while j <= 1000
+  4.times do
     i += j
     sum += numbers[i]
-
-    i += j
-    sum += numbers[i]
-
-    i += j
-    sum += numbers[i]
-
-    i += j
-    sum += numbers[i]
+  end
 
   j += 2
 end
