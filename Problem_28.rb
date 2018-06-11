6abd0044dbb1fcfec4701ccda480470182b92892
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-06-11 09:47:36 +0800

    Add another solution to problem 28

--- Problem_28.rb
+++ Problem_28.rb
@@ -1,5 +1,6 @@
 # Project Euler Problem 28
 
+=begin
 # Solution #1
 limit = 1001 ** 2
 numbers = (1..limit).to_a
@@ -14,4 +15,13 @@ while j <= 1000
   j += 2
 end
 
+puts sum
+=end
+
+# Solution #2
+sum = 1
+(3..1001).step(2).each do |n|
+  sum += n**2 + (n**2 - n + 1) \
+      + (n**2 - 2*n + 2) + (n**2 - 3*n + 3)
+end
 puts sum
