03d180ac33736c9de29af13f05ea17c207fcb963
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-07-03 20:25:56 +0800

    Finish problem 35

---
+++ Problem_35.rb
@@ -0,0 +1,25 @@
+# How many circular primes are there below one million?
+
+def is_prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).floor.downto(2).each do |i|
+    return false if num % i == 0
+  end
+  true
+end
+
+count = 0
+(2..1_000_000).each do |n|
+  index = n.to_s.length - 1
+  bool = true
+  (0..index).each do |j|
+    num = n.to_s.split(//).rotate(j).join.to_i
+    if not is_prime?(num)
+      bool = false
+      break
+    end
+  end
+  count += 1 if bool == true
+end
+
+puts count
